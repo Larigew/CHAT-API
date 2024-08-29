29091478b1a1e722dc97b8614259dcae830e399d
Larissa Gewehr <148505179+Larigew@users.noreply.github.com>
2024-08-28 23:13:11 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -14,7 +14,7 @@ app.use('/', router.get('/', (req, res, next) => {
 
 app.use('/', router.get('/sobre', (req, res, next) => {
     res.status(200).send({
-        "nome" : "FARCHAT",
+        "nome" : "CHAT",
         "autor" : "Lari Gewehr",
         "versao" : "0.1.0"
     });
